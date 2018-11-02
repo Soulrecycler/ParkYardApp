e807244b7f0744a3cea842394267ef276ba9da5d
Clinton <soares.clinton@gmail.com>
2018-11-02 19:55:09 +0530

    First Commit

--- app/src/main/java/com/application/parkyardapp/HomeActivity.java
+++ app/src/main/java/com/application/parkyardapp/HomeActivity.java
@@ -3,16 +3,26 @@ package com.application.parkyardapp;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class HomeActivity extends AppCompatActivity {
 
+    FloatingActionButton fab_plus,fab_settings,fab_logout;
+    Animation fab_open,fab_close,rotate_clockwise,rotate_anticlockwise;
+
+    Boolean fab_isOpen = false;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +32,22 @@ public class HomeActivity extends AppCompatActivity {
 
         final Button rentBtn,lendBtn;
 
-
-
-
         final Handler mHandler = new Handler();
+
+        firebaseAuth=FirebaseAuth.getInstance();
 
 
         rentBtn = findViewById(R.id.rentBtn);
         lendBtn = findViewById(R.id.lendBtn);
+
+        fab_plus = findViewById(R.id.fab_plus);
+        fab_settings = findViewById(R.id.fab_settings);
+        fab_logout = findViewById(R.id.fab_logout);
+
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        rotate_clockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_clockwise);
+        rotate_anticlockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_anticlockwise);
 
 
 
@@ -84,6 +102,42 @@ public class HomeActivity extends AppCompatActivity {
 
                 mHandler.postDelayed(intBookTimer,200);
 
+            }
+        });
+
+
+        fab_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(fab_isOpen)
+                {
+                    fab_settings.startAnimation(fab_close);
+                    fab_logout.startAnimation(fab_close);
+                    fab_plus.startAnimation(rotate_anticlockwise);
+                    fab_settings.setClickable(false);
+                    fab_logout.setClickable(false);
+                    fab_isOpen=false;
+                }
+                else
+                {
+                    fab_settings.startAnimation(fab_open);
+                    fab_logout.startAnimation(fab_open);
+                    fab_plus.startAnimation(rotate_clockwise);
+                    fab_settings.setClickable(true);
+                    fab_logout.setClickable(true);
+                    fab_isOpen=true;
+                }
+
+            }
+        });
+
+        fab_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(HomeActivity.this,MainActivity.class));
             }
         });
 
